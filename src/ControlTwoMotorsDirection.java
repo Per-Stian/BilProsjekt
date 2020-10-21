8d9d58fba0ce8c69f0bbc32d39b403d2dc1cc416
Per-Stian Alvestad <p-stian@hotmail.com>
2020-10-21 18:21:38 +0200

    Initial commit

---
+++ src/ControlTwoMotorsDirection.java
@@ -0,0 +1,29 @@
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.RaspiPin;
+/**
+ * Controls motor direction of a two DC motors
+ * @author https://javatutorial.net
+ */
+public class ControlTwoMotorsDirection {
+    public static void main(String[] args) throws InterruptedException {
+        // get a handle to the GPIO controller
+        final GpioController gpio = GpioFactory.getInstance();
+        final GpioPinDigitalOutput motor1pinA = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_23, "m1");
+        final GpioPinDigitalOutput motor2pinA = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_24, "m2");
+
+        System.out.println("rotate both motors clockwise for 3 seconds");
+        motor1pinA.high();
+        motor2pinA.high();
+
+        // wait 3 seconds
+        Thread.sleep(3000);
+        System.out.println("Stopping motor 1");
+        motor1pinA.low();
+        System.out.println("Stopping motor 2");
+        motor2pinA.low();
+        gpio.shutdown();
+    }
+}
+//fin
